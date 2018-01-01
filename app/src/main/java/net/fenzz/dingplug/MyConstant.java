afcee8906f2704a60e70c684fdeeba7ef1b057c4
lingdian <lingdian@meili-inc.com>
2018-01-01 23:07:28 +0800

    init commit

---
+++ app/src/main/java/net/fenzz/dingplug/MyConstant.java
@@ -0,0 +1,9 @@
+package net.fenzz.dingplug;
+
+/**
+ * Created by Administrator on 2017/12/30 0030.
+ */
+
+public class MyConstant {
+    public static final String ACTION_START_DING_SERVICE="action.start.ding.service";
+}
--- app/src/main/java/net/fenzz/dingplug/Utils.java
+++ app/src/main/java/net/fenzz/dingplug/Utils.java
@@ -2,6 +2,7 @@ package net.fenzz.dingplug;
 
 import java.util.List;
 
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -9,37 +10,69 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
+import android.provider.Settings;
+import android.view.accessibility.AccessibilityManager;
 
 public class Utils {
-	
-	public static void openCLD(String packageName,Context context) { 
-        PackageManager packageManager = context.getPackageManager(); 
-        PackageInfo pi = null;   
-         
-            try { 
-                 
-                pi = packageManager.getPackageInfo("com.alibaba.android.rimet", 0); 
-            } catch (NameNotFoundException e) { 
-                 
-            } 
-            Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null); 
-            resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER); 
-            resolveIntent.setPackage(pi.packageName); 
- 
-            List<ResolveInfo> apps = packageManager.queryIntentActivities(resolveIntent, 0); 
- 
-            ResolveInfo ri = apps.iterator().next(); 
-            if (ri != null ) { 
-                String className = ri.activityInfo.name; 
- 
-                Intent intent = new Intent(Intent.ACTION_MAIN); 
-                intent.addCategory(Intent.CATEGORY_LAUNCHER); 
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                ComponentName cn = new ComponentName(packageName, className); 
- 
-                intent.setComponent(cn); 
-                context.startActivity(intent); 
-            } 
-    } 
 
+    public static void openCLD(String packageName, Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo pi = null;
+
+        try {
+
+            pi = packageManager.getPackageInfo("com.alibaba.android.rimet", 0);
+        } catch (NameNotFoundException e) {
+
+        }
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(pi.packageName);
+
+        List<ResolveInfo> apps = packageManager.queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo ri = apps.iterator().next();
+        if (ri != null) {
+            String className = ri.activityInfo.name;
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            context.startActivity(intent);
+        }
+    }
+
+
+    /**
+     * 检查服务是否开启
+     * @param context
+     * http://blog.csdn.net/angcyo/article/details/51100898
+     * @return
+     */
+    public static boolean isServiceEnabled(Context context,Class className) {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> accessibilityServices =
+                accessibilityManager.getEnabledAccessibilityServiceList(
+                        AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().contains(className.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 跳转到
+     * http://blog.csdn.net/angcyo/article/details/51100898
+     * @param context
+     */
+    public static void jump2Setting(Context context){
+        //打开系统无障碍设置界面
+        Intent accessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        context.startActivity(accessibleIntent);
+    }
 }
